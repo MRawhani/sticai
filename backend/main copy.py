08c5192ac36efe0ea5a52e2b04a3c7cc73782213
mrawhani <mrawhani5@gmail.com>
2024-11-22 16:59:57 +0300

    add the image generayion code

--- backend/main copy.py
+++ backend/main copy.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import torch
-from diffusers import StableDiffusion3Pipeline,StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline
+from diffusers import StableDiffusion3Pipeline
 from huggingface_hub import login
 
 
@@ -10,11 +10,8 @@ import os
 app = FastAPI()
 
 # Define the local path to store the model
-xl_local_model_path = "./models/stable-diffusion-xl-base-1.0"
-refiner_local_model_path = "./models/stable-diffusion-xl-refiner-1.0"
-base_model_id = "stabilityai/stable-diffusion-xl-base-1.0"
-refiner_model_id = "stabilityai/stable-diffusion-xl-refiner-1.0"
-
+turbo_local_model_path = "./models/stable-diffusion-3.5-large-turbo"
+xl_local_model_path = "./models/stable-diffusion-3.5-large-turbo"
 pipe = None  # Global variable for the pipeline
 
 @app.get('/')
@@ -39,31 +36,20 @@ def load_pipeline():
     global pipe
     try:
         # Check if the model exists locally
-        if not os.path.exists(xl_local_model_path):
+        if not os.path.exists(turbo_local_model_path):
             print("Model not found locally. Downloading...")
             login(token="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")
 
-            pipe = StableDiffusionXLPipeline.from_pretrained(base_model_id,  torch_dtype=torch.float16, variant="fp16", use_safetensors=True)
-            pipe.save_pretrained(xl_local_model_path)  # Save the model locally for future use
-            print(f"Model downloaded and saved at {xl_local_model_path}")
+            pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large-turbo", torch_dtype=torch.bfloat16)
+            pipe.save_pretrained(turbo_local_model_path)  # Save the model locally for future use
+            print(f"Model downloaded and saved at {turbo_local_model_path}")
         else:
-            print(f"Loading model from local path: {xl_local_model_path}")
-            pipe = StableDiffusionXLPipeline.from_pretrained(xl_local_model_path, torch_dtype=torch.bfloat16)
-        # refiner checking
-        if not os.path.exists(refiner_local_model_path):
-            print("Refiner Model not found locally. Downloading...")
-
-            refiner_pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(refiner_model_id,  torch_dtype=torch.float16, variant="fp16", use_safetensors=True)
-            refiner_pipe.save_pretrained(refiner_local_model_path)  # Save the model locally for future use
-            print(f"Refiner Model downloaded and saved at {refiner_local_model_path}")
-        else:
-            print(f"Loading Refiner model from local path: {refiner_local_model_path}")
-            refiner_pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(refiner_local_model_path, torch_dtype=torch.bfloat16)
+            print(f"Loading model from local path: {turbo_local_model_path}")
+            pipe = StableDiffusionPipeline.from_pretrained(turbo_local_model_path, torch_dtype=torch.bfloat16)
 
         # Move the model to GPU if available, otherwise use CPU
         pipe.to("cuda" if torch.cuda.is_available() else "cpu")
-        refiner_pipe.to("cuda" if torch.cuda.is_available() else "cpu")
-        print("Pipelines loaded successfully")
+        print("Pipeline loaded successfully")
     except Exception as e:
         print(f"Error loading the pipeline: {e}")
 
